9d066a7214e69cbb206a15e1f885760e3ce9d804
Mysnikol <mysnikol@gmail.com>
2020-04-18 14:12:28 -0500

    speedRange (): subspecies

--- src/Fox.java
+++ src/Fox.java
@@ -4,10 +4,40 @@ public class Fox extends Canine {
     boolean isOmnivore;
 
     public void tchoffo(boolean isTchoffing) {
-
+        solitary = true;
+        isOmnivore = true;
     }
 
     public void Prey(String[] args) {
+        String prey = new Prey[];
+    }
+
+    public speedRange(int max, int min) {
+        if (max > min || (max - min + 1 > Integer.MAX_VALUE)) {
+            throw new IllegalArgumentException("Invalid Range");
+            {
+
+//            int speed =
+//            int low = 30, high = 40;
+
+                for (int i = 0; i > 30 && i < 40; i++) {
+                    System.out.println(speedRange(high, low));
+                }
+            }
+        }
+    }
+
+//    {
+//        if (max > min || (max - min + 1 > Integer.MAX_VALUE)) {
+//            throw new IllegalArgumentException("Invalid Range");
+//        }
+//        return new Random().nextInt(max - min + 1) + min;
+//
+//    }
+
+//        public static void main (String[]args){
+//
+//        }
 
     }
 }
